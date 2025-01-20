fd2748de525267805661d19e23faaf792ed4dc07
airreality <airunreality@gmail.com>
2025-01-20 22:54:40 +0800

    improve initial logic

--- air_waiter/wait.py
+++ air_waiter/wait.py
@@ -5,26 +5,34 @@ import time
 from contextlib import suppress
 from functools import partial
 from time import sleep
-from typing import Any, Literal, TYPE_CHECKING, TypeVar
+from typing import Any, Literal, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from collections.abc import Callable
 
-T = TypeVar("T")
+
+class UnlimitedWaiterError(Exception):
+    def __init__(self) -> None:
+        super().__init__("Wrong waiter configuration: endless timeout is not allowed with not limited attempts")
+
+
+class UnusedMaxIntervalError(Exception):
+    def __init__(self) -> None:
+        super().__init__("Wrong waiter configuration: max_interval should be used with is_exponential `True`")
 
 
 class Wait[T]:
     def __init__(  # noqa: PLR0913
         self,
         action: Callable[..., T],
-        *args: Any,  # noqa: ANN401
+        *args: Any,
         timeout: float,
         max_attempts: int = 0,
         exceptions_to_ignore: tuple[type[Exception]] = (),  # type: ignore[assignment]
         interval: float = 0.1,
         is_exponential: bool = False,
         max_interval: float = 0,
-        **kwargs: Any,  # noqa: ANN401
+        **kwargs: Any,
     ) -> None:
         """Waiter logic class to call a callable until expected result.
 
@@ -34,7 +42,8 @@ class Wait[T]:
 
         :param action: Callable to call
         :param args: Positional args for the action
-        :param timeout: Maximal time in seconds to wait. 0 to wait without limit by time
+        :param timeout: Maximal time in seconds to wait. 0 to wait without limit by time.
+            0 is allowed only with max_attempts != 0.
         :param max_attempts: Maximal calls count. 0 to call without limit by count
         :param exceptions_to_ignore: Exceptions which will be ignored if happen during the action call
         :param interval: Polling interval in seconds between calls of an action
@@ -44,10 +53,18 @@ class Wait[T]:
         :param kwargs: Keyword args for the action
         """
         self._action = partial(action, *args, **kwargs)
+
+        if timeout <= 0 and max_attempts <= 0:
+            raise UnlimitedWaiterError
+
         self._timeout = timeout
         self._max_attempts = max_attempts
         self._interval = interval
         self._exceptions_to_ignore = exceptions_to_ignore
+
+        if not is_exponential and max_interval != 0:
+            raise UnusedMaxIntervalError
+
         self._is_exponential = is_exponential
         self._max_interval = max_interval
 
@@ -63,15 +80,18 @@ class Wait[T]:
 
         while True:
             remaining_time = end_time - time.time()
-            if (self._timeout != 0 and remaining_time < 0) or self._calls_count >= self._max_attempts:
+            if (self._timeout > 0 and remaining_time < 0) or (
+                self._max_attempts != 0 and self._calls_count >= self._max_attempts
+            ):
                 break
 
-            delay = (
-                min(self._interval * (2**self._calls_count), self._max_interval)
-                if self._is_exponential
-                else self._interval
-            )
-            sleep(min(delay, remaining_time))
+            if self._is_exponential:
+                interval = self._interval * (2**self._calls_count)
+                delay = min(interval, self._max_interval) if self._max_interval != 0 else interval
+            else:
+                delay = self._interval
+
+            sleep(min(delay, remaining_time) if self._timeout > 0 else delay)
             self._calls_count += 1
 
             with suppress(*self._exceptions_to_ignore):
@@ -80,11 +100,11 @@ class Wait[T]:
                 if predicate(result):
                     return result
 
-        msg = f"Waiter timeout after {self._calls_count} action calls with results {result}"
+        msg = f"Waiter timeout after {self._calls_count} action calls with results {self._results}"
         raise TimeoutError(msg)
 
-    def until(self) -> T:
-        return self._poll(predicate=operator.truth)
+    def until(self, predicate: Callable[[T], bool] | None = None) -> T:
+        return self._poll(predicate=predicate or operator.truth)
 
     def until_not(self) -> T:
         return self._poll(predicate=operator.not_)
@@ -95,17 +115,17 @@ class Wait[T]:
     def until_not_equal_to(self, value: T) -> T:
         return self._poll(predicate=partial(operator.ne, value))
 
-    def until_true(self) -> Literal[True]:
+    def until_is_true(self) -> Literal[True]:
         return self._poll(predicate=partial(operator.is_, True))  # type: ignore[return-value]  # noqa: FBT003
 
-    def until_false(self) -> Literal[False]:
+    def until_is_false(self) -> Literal[False]:
         return self._poll(predicate=partial(operator.is_, False))  # type: ignore[return-value]  # noqa: FBT003
 
-    def until_none(self) -> None:
+    def until_is_none(self) -> None:
         return self._poll(predicate=partial(operator.is_, None))  # type: ignore[return-value]
 
-    def until_not_none(self) -> T:
+    def until_is_not_none(self) -> T:
         # TODO: is it possible to constraint this method annotation that it never returns none?
         # without passing action as method argument?
-        # self.action: Callable[..., T | None]
+        # action: Callable[..., T | None]
         return self._poll(predicate=partial(operator.is_not, None))
